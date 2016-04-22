2a2b88f821479315076e29d37c703d92078377a6
Roberto Bampi <bampi.roberto@gmail.com>
2016-04-22 11:39:03 +0200

    media files are now served correctly

--- scontrini/urls.py
+++ scontrini/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
 from django.contrib import admin
+
 
 from scontrini.main.views import MainView, UploadView, ReceiptUpdate, ReceiptListView
 
@@ -24,4 +27,5 @@ urlpatterns = [
     url(r'^upload/$', UploadView.as_view()),
     url(r'^edit/(?P<pk>\d+)$', ReceiptUpdate.as_view()),
     url(r'^$', ReceiptListView.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
