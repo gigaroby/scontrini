341352ef820222b8d2a2be08165cf2b81090c169
Roberto Bampi <bampi.roberto@gmail.com>
2016-04-22 11:32:05 +0200

    file upload works properly

--- scontrini/main/models.py
+++ scontrini/main/models.py
@@ -3,11 +3,19 @@ from django.contrib.auth.models import User
 from jsonfield import JSONField
 
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.owner.id, filename)
+
+
 class Receipt(models.Model):
     owner = models.ForeignKey(to=User)
 
-    image = models.ImageField()
+    image = models.ImageField(upload_to=user_directory_path)
     receipt_data = JSONField(blank=True)
 
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "<Receipt owner={} created={}>".format(self.owner.username, self.created)
 
--- scontrini/main/views.py
+++ scontrini/main/views.py
@@ -1,10 +1,14 @@
+import io
+import time
+import base64
+
+from django.core.files import File
 from django.http import HttpResponse
 from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms.models import ModelForm
 
 from .models import Receipt
-
 
 
 class MainView(TemplateView):
@@ -21,5 +25,11 @@ class UploadView(LoginRequiredMixin, View):
     def post(self, request):
         remove = 'imgData=data:image/png;base64,'
         image_base64 = request.body[len(remove):]
+        image_data = io.BytesIO(base64.b64decode(image_base64))
 
-        return HttpResponse(b'merda')
+        new_receipt = Receipt()
+        new_receipt.owner = request.user
+        new_receipt.image.save('{}.png'.format(time.monotonic()), File(image_data))
+        new_receipt.save()
+
+        return HttpResponse(new_receipt.id)
