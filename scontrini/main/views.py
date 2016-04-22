7c61670f21ae1825baeaa354fa0ae2b2011a395b
Davide Setti <davide.setti@gmail.com>
2016-04-22 11:29:14 +0200

    bootstrap

--- scontrini/main/views.py
+++ scontrini/main/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms.models import ModelForm
 from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
 
 from .models import Receipt
 
@@ -29,3 +30,7 @@ class ReceiptUpdate(UpdateView):
     model = Receipt
     fields = ['name', 'category', 'price', 'notes']
     template_name_suffix = '_update_form'
+
+
+class ReceiptListView(ListView):
+    model = Receipt
