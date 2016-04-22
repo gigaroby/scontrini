d80ff55d55a9f7dda48061ce0613cd178798243b
ugoscaiella <scaiella@spaziodati.eu>
2016-04-22 14:41:13 +0200

    implemented first match by good vat

--- scontrini/ocr/ocr.py
+++ scontrini/ocr/ocr.py
@@ -1,5 +1,7 @@
 import json
 import math
+
+import jmespath
 import re
 
 import requests
@@ -8,6 +10,18 @@ import tempfile
 from PIL import Image
 from django.conf import settings
 import os
+
+
+def api_url(func):
+    return "https://api-u.spaziodati.eu/{}?token={}".format(func, settings.SD_TOKEN)
+
+def check_resp(resp):
+    if resp.status_code != 200:
+        raise Exception("URL [{}] got status {} with content: {}".format(
+            resp.url,
+            resp.status_code,
+            resp.content
+        ))
 
 
 class OcrReceipt(object):
@@ -44,8 +58,7 @@ class OcrReceipt(object):
                     data={'apikey': settings.OCR_API_KEY, 'language': 'ita'},
                     files={'receipt': f}
                 )
-                if resp.status_code != 200:
-                    raise Exception('OCR API returned {}: {}'.format(resp.status_code, resp.content))
+                check_resp(resp)
 
                 result = resp.json()
                 if result['IsErroredOnProcessing']:
@@ -55,3 +68,53 @@ class OcrReceipt(object):
                 return result['ParsedResults'][0]['ParsedText']
         finally:
             os.remove(fname)
+
+    def get_company_list(self):
+        ocr_text = self.get_ocr_data()
+
+        good_vat = self.search_for_piva(ocr_text)
+        if good_vat:
+            return self.anagrafica_by_vat(good_vat)
+        else:
+            return []
+
+    def anagrafica_by_vat(self, vat):
+        resp = requests.get(api_url('azienda/anagrafica')+'&piva={}'.format(vat))
+        check_resp(resp)
+        result = resp.json()
+        if result['meta']['items'] == 0:
+            return None
+        else:
+            item = result['results'][0]
+            return [{
+                'label': item['denominazione'],
+                'province': item['sede']['provincia'],
+                'atoka_link': item['link_atoka'],
+                'sector': item['descrizione'],
+                'vat': item['piva']
+            }]
+
+    def search_for_piva(self, text):
+        resp = requests.post(
+            api_url('datatxt/nex/v1'),
+            data={
+                'include': 'types',
+                'extra_types': 'vat',
+                'text': text,
+                'country': 'it',
+                'min_confidence': 0
+            }
+        )
+        check_resp(resp)
+        result = resp.json()
+        vatcodes = jmespath.search(
+            "annotations[?types == ['http://dandelion.eu/ontology#vatID']].title",
+            result
+        )
+        if vatcodes:
+           return vatcodes[0][2:]
+        else:
+            return None
+
+
+
